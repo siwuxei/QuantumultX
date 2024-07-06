22b380f75fca862d94e94803e27bd91097e4f8eb
Li Lele <50586809+siwuxei@users.noreply.github.com>
2024-07-07 02:42:56 +0800

    更新 revenuecat.js

--- qx/revenuecat.js
+++ qx/revenuecat.js
@@ -21,7 +21,7 @@ https://appraven.net/collection/77299969
 
 # ～ RevenueCat@ddgksf2013
 ^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/[^/]+$) url script-response-body https://raw.githubusercontent.com/siwuxei/QuantumultX/main/qx/revenuecat.js
-^https:\/\/api\.revenuecat\.com\/.+\/(receipts|subscribers) url script-request-header https://raw.githubusercontent.com/ddgksf2013/Scripts/master/deleteHeader.js
+^https:\/\/api\.revenuecat\.com\/.+\/(receipts|subscribers) url script-request-header https://raw.githubusercontent.com/siwuxei/QuantumultX/main/qx/deleteHeader.js
 
 [mitm]
 
