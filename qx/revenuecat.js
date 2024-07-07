db017ab7784e5c51d8555e9ad5c7e0e7846155e8
Li Lele <50586809+siwuxei@users.noreply.github.com>
2024-07-07 14:48:53 +0800

    Update revenuecat.js

--- qx/revenuecat.js
+++ qx/revenuecat.js
@@ -193,4 +193,41 @@ const mapping = {
 
 // =========    固定部分  ========= // 
 // =========  @ddgksf2021 ========= // 
-var ua=$request.headers["User-Agent"]||$request.headers["user-agent"],obj=JSON.parse($response.body);obj.Attention="恭喜你抓到元数据！由墨鱼分享，请勿售卖或分享他人！";var ddgksf2013={is_sandbox:!1,ownership_type:"PURCHASED",billing_issues_detected_at:null,period_type:"normal",expires_date:"2099-12-18T01:04:17Z",grace_period_expires_date:null,unsubscribe_detected_at:null,original_purchase_date:"2022-09-08T01:04:18Z",purchase_date:"2022-09-08T01:04:17Z",store:"app_store"},ddgksf2021={grace_period_expires_date:null,purchase_date:"2022-09-08T01:04:17Z",product_identifier:"com.ddgksf2013.premium.yearly",expires_date:"2099-12-18T01:04:17Z"};const match=Object.keys(mapping).find(e=>ua.includes(e));if(match){let[e,s]=mapping[match];s?(ddgksf2021.product_identifier=s,obj.subscriber.subscriptions[s]=ddgksf2013):obj.subscriber.subscriptions["com.ddgksf2013.premium.yearly"]=ddgksf2013,obj.subscriber.entitlements[e]=ddgksf2021}else obj.subscriber.subscriptions["com.ddgksf2013.premium.yearly"]=ddgksf2013,obj.subscriber.entitlements.pro=ddgksf2021;$done({body:JSON.stringify(obj)});
+var ua=$request.headers["User-Agent"]||$request.headers["user-agent"],obj=JSON.parse($response.body);
+
+obj.Attention="恭喜你抓到元数据！由墨鱼分享，请勿售卖或分享他人！";
+
+var ddgksf2013 = {
+    is_sandbox:!1,
+    ownership_type:"PURCHASED",
+    billing_issues_detected_at:null,
+    period_type:"normal",
+    expires_date:"2099-12-18T01:04:17Z",
+    grace_period_expires_date:null,
+    unsubscribe_detected_at:null,
+    original_purchase_date:"2022-09-08T01:04:18Z",
+    purchase_date:"2022-09-08T01:04:17Z",
+    store:"app_store"
+};
+
+var ddgksf2021 = {
+    grace_period_expires_date:null,
+    purchase_date:"2022-09-08T01:04:17Z",
+    product_identifier:"com.ddgksf2013.premium.yearly",
+    expires_date:"2099-12-18T01:04:17Z"
+};
+
+const match = Object.keys(mapping).find(e => ua.includes(e));
+
+if (match) {
+    let [e, s] = mapping[match];
+    if (s) {
+        ddgksf2021.product_identifier = s;
+        obj.subscriber.subscriptions[s] = ddgksf2013;
+    } else {
+        obj.subscriber.subscriptions["com.ddgksf2013.premium.yearly"] = ddgksf2013;
+    }
+    obj.subscriber.entitlements[e] = ddgksf2021;
+}
+
+$done({body: JSON.stringify(obj)});
