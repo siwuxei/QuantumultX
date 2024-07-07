0e630322410437d9e7429d45a6a368e49d6fb8af
Li Lele <50586809+siwuxei@users.noreply.github.com>
2024-07-07 17:04:35 +0800

    Update revenuecat.js

--- qx/revenuecat.js
+++ qx/revenuecat.js
@@ -226,7 +226,7 @@ var ddgksf2021 = {
 const match = Object.keys(mapping).find(e => ua.includes(e));
 
 if (match) {
-    $notify('Match Result', '', match);
+    console.log("Hello, world!");
     let [e, s] = mapping[match];
     if (s) {
         ddgksf2021.product_identifier = s;
